367eef3431bb2252c4e40d5bc76659386ce89d23
Chu <cnpanic@naver.com>
2018-02-01 22:11:17 +0900

    Alamofire 섭딱 통신 완료

--- AFSample/ViewController.swift
+++ AFSample/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//       Alamofire.request(.GET, "https://codewithchris.com/code/afsample.json").responseJSON { response in --> 구문법
+        Alamofire.request("https://codewithchris.com/code/afsample.json").responseJSON { response in
+//        Alamofire.request("https://new1208.herokuapp.com/book_content/1").responseJSON { response in
+      
+        if let JSON = response.result.value {
+            let json = JSON as! [String: Any]
+            print(json["firstkey"])
+            print(json["secondkey"])
+            
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
